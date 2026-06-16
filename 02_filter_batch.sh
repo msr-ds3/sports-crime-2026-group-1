b926eaf462a4e77130e15feab28909d07538eeb2
Jacqueline Louie <jacquelineyylouie@gmail.com>
2026-06-16 12:00:15 -0400

    Scripts to unzip data from ICPSR + Makefile (optional)

---
+++ 02_filter_batch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Extract and process the data for offense segments for years
+# 2000 to 2005 
+# File format inside the offense_segment_csv_1991_2024.zip
+# nibrs_offense_segment_<year>.csv
+# Link where data was retrieved: https://www.openicpsr.org/openicpsr/project/118281/version/V11/view?path=/openicpsr/118281/fcr:versions/V11/batch_header_csv_1991_2024.zip&type=file 
+
