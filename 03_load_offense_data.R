946fbf54baf94600d4b6525e2ef172c8394665ff
Jacqueline Louie <jacquelineyylouie@gmail.com>
2026-06-17 13:40:28 -0400

    work so far

--- 03_load_offense_data.R
+++ 03_load_offense_data.R
@@ -8,13 +8,19 @@ batch_data <- select(batch_data, ori, year, state, population, city_name, agency
 
 school_info_tb <- tibble(
     city_name = c("fayetteville","jonesboro","colorado springs","boise","moscow","ames","iowa city","lawrence","ann arbor","east lansing","kalamazoo","mount pleasant","akron","athens","columbus","clemson","columbia","murfreesboro","austin","denton","lubbock","logan","provo","blacksburg","huntington","morgantown"),
+    home_team_full = c("Arkansas Razorbacks", "Arkansas State Red Wolves", "Air Force Falcons", "Boise State Broncos", "Idaho Vandals", "Iowa State Cyclones", "Iowa Hawkeyes", "Kansas Jayhawks", 
+    "Michigan Wolverines", "Michigan State Spartans", "Western Michigan Broncos", "Eastern Michigan Eagles", "Akron Zips", "Ohio Bobcats", "Ohio State Buckeyes", "Clemson Tigers", "South Carolina Gamecocks",
+     "Middle Tennessee Blue Raiders", "Texas Longhorns", "North Texas Mean Green", "Texas Tech Red Raiders", "Utah State Aggies", "BYU Cougars", "Virginia Tech Hokies", "Marshall Thundering Herd", 
+     "West Virginia Mountaineers"), 
     school = c("University of Arkansas", "Arkansas State University", "United States Air Force Academy", "Boise State University", "The University of Idaho", "Iowa State University", "The University of Iowa", "The University of Kansas", "The University of Michigan", "Michigan State University", "Western Michigan University", "Eastern Michigan University", "The University of Akron", "Ohio University", "The Ohio State University", "Clemson University", "The University of South Carolina", "Middle Tennessee State University", "The University of Texas at Austin", "The University of North Texas", "Texas Tech University", "Utah State University", "Brigham Young University", "Virginia Polytechnic Institute and State University", "Marshall University", "West Virginia University"),
     state = c("arkansas", "arkansas", "colorado", "idaho", "idaho", "iowa", "iowa", "kansas", "michigan", "michigan", "michigan", "michigan", "ohio", "ohio", "ohio", "south carolina", "south carolina", "tennessee", "texas", "texas", "texas", "utah", "utah", "virginia", "west virginia", "west virginia")
 )
 
 ori_per_school <- inner_join(school_info_tb, batch_data, by = c("city_name", "state")) %>% 
-    filter(agency_indicator == "city") %>%
-    select(city_name, school, state, ori)
+    # filter(agency_indicator == "city") %>%
+    select(city_name, school, state, ori, home_team_full) 
+
+ori_schools <- tibble(ori_per_school$ori)
 
 ## Load Offense Data
 test_crime_data <- read_csv("nibrs_offense_segment_2000_2005.csv")
@@ -27,7 +33,8 @@ grade_a_offenses <-filter(test_crime_data,
 ) %>% 
 select(ori, ucr_offense_code, incident_date)
 
-a_offenses_per_ori <- inner_join(ori_per_school, grade_a_offenses, by = "ori") %>%
+a_offenses_per_ori <- left_join(ori_per_school, grade_a_offenses, by = "ori") %>%
     mutate(offense_type = ifelse(ucr_offense_code == "destruction/damage/vandalism of property", "vandalism", "assault")) %>%
-    filter(year(incident_date) >= 2000 & year(incident_date) <= 2005) %>%
-    select(ori, school, offense_type, city_name, state, incident_date) 
+    filter(year(incident_date) >= 2000 & year(incident_date) <= 2005) # %>%
+    select(ori, school, home_team_full, offense_type, city_name, state, incident_date) %>%
+    group_by(ori) %>% summarize()
--- 04_load_football_college.R
+++ 04_load_football_college.R
@@ -14,31 +14,34 @@ schedules <- map_dfr(years, function(yr){
 school_schedule <- tibble(
     home_team_location = c("Arkansas", "Arkansas State" ,"Air Force", "Boise State", "Idaho","Iowa State", "Iowa", "Kansas","Michigan", 
     "Michigan State","Western Michigan","Eastern Michigan","Akron", "Ohio","Ohio State","Clemson", "USC", "Middle Tennessee","Texas","North Texas","Texas Tech","Utah State","BYU","Virginia Tech","Marshall","West Virginia"),
-    team_full_name = c("Akron Zips", "Iowa State Cyclones", "Michigan Wolverines", "Ohio Bobcats", "Texas Longhorns", "Virginia Tech Hokies", "Boise State Broncos", "Clemson Tigers", 
-    "Air Force Falcons", "South Carolina Gamecocks", "Ohio State Buckeyes", "North Texas Mean Green", "Michigan State Spartans", "Arkansas Razorbacks", "Marshall Thundering Herd", 
-    "Iowa Hawkeyes", "Arkansas State Red Wolves", "Western Michigan Broncos", "Kansas Jayhawks", "Utah State Aggies", "Texas Tech Red Raiders", "West Virginia Mountaineers", 
-    "Idaho Vandals", "Eastern Michigan Eagles", "Middle Tennessee Blue Raiders", "BYU Cougars"), # not aligned with
+    home_team_full = c("Arkansas Razorbacks", "Arkansas State Red Wolves", "Air Force Falcons", "Boise State Broncos", "Idaho Vandals", "Iowa State Cyclones", "Iowa Hawkeyes", "Kansas Jayhawks", 
+    "Michigan Wolverines", "Michigan State Spartans", "Western Michigan Broncos", "Eastern Michigan Eagles", "Akron Zips", "Ohio Bobcats", "Ohio State Buckeyes", "Clemson Tigers", "South Carolina Gamecocks",
+     "Middle Tennessee Blue Raiders", "Texas Longhorns", "North Texas Mean Green", "Texas Tech Red Raiders", "Utah State Aggies", "BYU Cougars", "Virginia Tech Hokies", "Marshall Thundering Herd", 
+     "West Virginia Mountaineers"), 
     city_name = c("fayetteville","jonesboro","colorado springs","boise","moscow","ames","iowa city","lawrence","ann arbor","east lansing","kalamazoo","mount pleasant","akron","athens","columbus","clemson","columbia","murfreesboro","austin","denton","lubbock","logan","provo","blacksburg","huntington","morgantown"),
     school_name = c("University of Arkansas", "Arkansas State University", "United States Air Force Academy", "
     Boise State University", "The University of Idaho", "Iowa State University", "The University of Iowa", "The University of Kansas", "The University of Michigan", "Michigan State University", "Western Michigan University", "Eastern Michigan University", "The University of Akron", "Ohio University", 
     "The Ohio State University", "Clemson University", "The University of South Carolina", "Middle Tennessee State University", "The University of Texas at Austin", "The University of North Texas", "Texas Tech University", "Utah State University", 
-    "Brigham Young University", "Virginia Polytechnic Institute and State University", "Marshall University", "West Virginia University"),
-    state = c("arkansas", "arkansas", "colorado", "idaho", "idaho", "iowa", "iowa", "kansas", "michigan", "michigan", "michigan", "michigan", "ohio", "ohio", "ohio", "south carolina", "south carolina", "tennessee", "texas", "texas", "texas", "utah", "utah", "virginia", "west virginia", "west virginia")
+    "Brigham Young University", "Virginia Polytechnic Institute and State University", "Marshall University", "West Virginia University")#,
+    #state = c("arkansas", "arkansas", "colorado", "idaho", "idaho", "iowa", "iowa", "kansas", "michigan", "michigan", "michigan", "michigan", "ohio", "ohio", "ohio", "south carolina", "south carolina", "tennessee", "texas", "texas", "texas", "utah", "utah", "virginia", "west virginia", "west virginia")
 )
 
-schedule_teams_by_loc <- schedules %>%
-    filter(home_team_location %in% school_schedule$home_team_location | away_team_location %in% school_schedule$home_team_location)
-
 schedule_teams_by_name <- schedules %>%
-    filter(home_team_full %in% school_schedule$team_full_name | away_team_full %in% school_schedule$team_full_name)
+    filter(home_team_full %in% school_schedule$home_team_full) %>% 
+    mutate(is_home = (home_team_full %in% school_schedule$home_team_full), date = game_date) %>%
+    select(date, matchup, home_team_location, home_team_full, away_team_location, away_team_full, is_home)
 
+# Home Games
 a_offenses_per_ori <- a_offenses_per_ori %>% rename(date = incident_date)
+data <- left_join(schedule_teams_by_name, school_schedule, by = "home_team_full")
+a_offenses_per_ori
 
-#Crime on game days vs. non-game days
-schedules_extended <- schedules %>% rename(date = game_date) %>% 
-    inner_join(school_schedule, by = c("home_team_location"))
+pos_cor_2 <- left_join(a_offenses_per_ori, data, by = c("date", "city_name"))
 
-data <- a_offenses_per_ori %>% left_join(schedules_extended, by = c("city_name", "date"))
+possibly_correct_join <- group_by(a_offenses_per_ori, date, city_name) # %>% 
+    summarize(offense_count = n()) #%>%
+    left_join(schedule_teams_by_name, by = c("date", "home_team_full"))
+
 
 # Daily counts per agency 
 data %>% group_by(date,city_name)  %>% summarize (count = n())
